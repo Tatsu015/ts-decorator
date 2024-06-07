b1c0dae98d10a51f66a6c6bb54c1011ce7628ba3
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-06-07 21:33:43 +0900

    add decorator sample

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
-function hello(name: string) {
-    console.log(`hello ${name}`);
+function deco(value: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+        console.log(value);
+    };
 }
 
-hello('alice')
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    @deco("aaa")
+    greet1() {
+        return "Hello, " + this.greeting;
+    }
+    @deco("bbb")
+    greet2() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+const g = new Greeter('xxx');
+console.log(g.greet1())
+console.log(g.greet2())
